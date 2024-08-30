1e77c55fff2e3940df96f23f6beb59e00c3f1e8a
Oziel Sousa <ozielzin358@gmail.com>
2024-08-30 16:38:42 -0300

    feat: shows api response in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { ArrowUp } from "lucide-react";
+import { useState, useRef } from "react";
 // import {DiffEditor, Editor} from "@monaco-editor/react"
 import Groq from "groq-sdk";
 
@@ -8,18 +9,23 @@ const groq = new Groq({
 });
 
 export function Home() {
+  const [generatedCode, setGeneratedCode] = useState();
+  const inputRef = useRef() 
+  
   async function startQuery() {
-    const chatCompletion = await getGroqChatCompletion();
-    // Print the completion returned by the LLM.
+    
+    const chatCompletion = await getGroqChatCompletion();  
     console.log(chatCompletion.choices[0]?.message?.content || "");
+    setGeneratedCode(chatCompletion.choices[0]?.message?.content || "");
   }
-
+  
   async function getGroqChatCompletion() {
+    const userInput = inputRef.current.value
     return groq.chat.completions.create({
       messages: [
         {
           role: "user",
-          content: "Descreva sobre Goiânia -Go",
+          content: `${userInput}`,
         },
       ],
       model: "llama3-8b-8192",
@@ -29,18 +35,21 @@ export function Home() {
   return (
     <div className="flex flex-col items-center justify-between min-h-screen bg-gray-900 text-white">
       <div className="w-full max-w-2xl p-4 overflow-y-auto h-4/5 bg-transparent rounded-lg ">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla neque
-          et ipsa iusto maxime quod ratione quas saepe minima fugit. Vitae
-          aliquid voluptates ducimus possimus a veniam quisquam cupiditate quia?
-        </p>
+        <pre>
+          {generatedCode}
+        </pre>
       </div>
       <div className="w-full max-w-2xl p-4 relative">
         <input
+          name="messageInput"
           className="w-full p-4 pl-10 pr-12 outline-none text-white bg-gray-600  rounded-full mb-2"
           placeholder="Digite sua mensagem..."
+          ref={inputRef}
         />
-        <button onClick={()=> startQuery()} className="absolute bg-gray-400 rounded-full right-7  top-1/3 transform -translate-y-[14%] text-gray-900">
+        <button
+          onClick={() => startQuery()}
+          className="absolute bg-gray-400 rounded-full right-7  top-1/3 transform -translate-y-[14%] text-gray-900"
+        >
           <ArrowUp size={30} strokeWidth={1.4} />
         </button>
       </div>
